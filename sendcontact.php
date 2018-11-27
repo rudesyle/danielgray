e93ffc3e3a918aed1ea6bf0685a633ad7eb1b69b
Jeffrey Rudesyle <jrudesyle@rtmsmail.com>
2018-11-26 20:09:06 -0500

    Latest updates

---
+++ sendcontact.php
@@ -0,0 +1,18 @@
+<?php
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
+    set_error_handler("var_dump");
+
+    $date = date_create()->format('mmddYYhhmmss');
+    $fileName = "ContactRequest_" . $date . ".txt";
+
+    $contactFile = fopen($fileName, "a") or die("Unable to open file!");
+    $txt = "Name is : " . $_POST["contact_name"] . "\n";
+    fwrite($contactFile, $txt);
+    $txt = "Email is : " . $_POST["contact_email"] . "\n";
+    fwrite($contactFile, $txt);
+    $txt = "Message is : " . $_POST["contact_msg"] . "\n";
+    fwrite($contactFile, $txt);
+    fclose($contactFile);
+    
+?> 
